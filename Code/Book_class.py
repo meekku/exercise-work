9f0fce3aefb688421817e9d0b7fbce951c801873
Nea Träskman <75360706+NeaTr@users.noreply.github.com>
2022-03-28 12:19:11 +0300

    Added a parent and child class
    Loan parent class and book child class

---
+++ Code/Book_class.py
@@ -0,0 +1,9 @@
+import Loan_class
+
+class Book(Loan_class.Loan):
+
+    def __init__(self):
+        super().__init__()
+    
+    def __str__(self):
+        return super().__str__()
---
+++ Code/Loan_class.py
@@ -0,0 +1,8 @@
+
+class Loan():
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
