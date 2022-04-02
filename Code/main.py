4e4849f0ca1743514799b7bca5ab33121fcc4700
Nea Träskman <nea.traskman@gmail.com>
2022-04-02 17:14:04 +0300

    Update main.py
    Added first prototype of our login page

--- Code/main.py
+++ Code/main.py
@@ -5,6 +5,7 @@
 
 import User_class
 import Loan_class
+from tkinter import *
 
 
 def main():
@@ -14,6 +15,29 @@ def main():
 
 
     print(library)
+
+
+
+def main_login_screen():
+
+    main_screen = Tk() # creating the GUI
+    main_screen.geometry("300x250") # setting the size of the GUI window
+    main_screen.title("Library - Login") # title of the GUI window
+
+    #creating the form label
+    Label(text="Login or Register", bg="gray", width="300", height="2", font=("Calibri", 12)).pack()
+    Label(text="").pack()
+
+    # creating the login button
+    Button(text="Login", height="2", width="30").pack()
+    Label(text="").pack()
+
+    # creating the register button
+    Button(text="Register", height="2", width="30").pack()
+
+    main_screen.mainloop() # starts the GUI
+
+main_login_screen()
 
 
 
