2f479c31d03eca319d5c0d9fe2cf667cb0d0e91b
Nea Träskman <nea.traskman@gmail.com>
2022-04-02 19:12:57 +0300

    Removed extra spaces

--- Code/main.py
+++ Code/main.py
@@ -2,15 +2,12 @@
 # Author: Melina Kamunen and Nea Träskman
 # Description: 
 
-
 import User_class
 import Loan_class
 import Gui_class
 
-
 def main():
-
-
+    
     library = User_class.User("Nea", "Träskman")
     print(library)
 
