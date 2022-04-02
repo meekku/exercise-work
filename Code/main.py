ae4a116fc12e4ad6c45748fab68065c0ba524e10
Nea Träskman <nea.traskman@gmail.com>
2022-04-02 17:38:26 +0300

    Moved GUI from main to its own file

--- Code/main.py
+++ Code/main.py
@@ -5,40 +5,17 @@
 
 import User_class
 import Loan_class
-from tkinter import *
+import Gui_class
 
 
 def main():
 
 
     library = User_class.User("Nea", "Träskman")
-
-
     print(library)
 
-
-
-def main_login_screen():
-
-    main_screen = Tk() # creating the GUI
-    main_screen.geometry("300x250") # setting the size of the GUI window
-    main_screen.title("Library - Login") # title of the GUI window
-
-    #creating the form label
-    Label(text="Login or Register", bg="gray", width="300", height="2", font=("Calibri", 12)).pack()
-    Label(text="").pack()
-
-    # creating the login button
-    Button(text="Login", height="2", width="30").pack()
-    Label(text="").pack()
-
-    # creating the register button
-    Button(text="Register", height="2", width="30").pack()
-
-    main_screen.mainloop() # starts the GUI
-
-main_login_screen()
-
+    app = Gui_class.App()
+    app.mainloop()
 
 
 if __name__ == "__main__":
