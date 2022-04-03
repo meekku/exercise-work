28e72992f74c0dfe2334f9edfa68e47a574e19af
Nea Träskman <nea.traskman@gmail.com>
2022-04-03 20:34:34 +0300

    Added login and register options

--- Code/Gui_class.py
+++ Code/Gui_class.py
@@ -39,7 +39,6 @@ class App(tk.Tk):
         frame = self.frames[page]
         frame.tkraise()
 
-
 class HomePage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -69,7 +68,6 @@ class HomePage(tk.Frame):
         command=lambda: controller.show_frame(Page_Register))
         Button_Register.pack(pady=10)
 
-
 class Page_Login(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -81,6 +79,27 @@ class Page_Login(tk.Frame):
 
         text2_label = tk.Label(self, text="E-Library Login", bg="white", fg="black", font=('Helvetica', '32', 'bold'))
         text2_label.pack(pady=10,padx=10)
+
+        username_login = StringVar()
+        password_login = StringVar()
+
+        login_label = tk.Label(self, text="Please enter details below to login", bg="white", fg="black", 
+        font=('Helvetica', '16'))
+        login_label.pack(pady=10)
+
+        Label(self, text="Username * ", bg="white", fg="black").pack()
+        username_login_entry = tk.Entry(self, textvariable=username_login, bg="white", fg="black")
+        username_login_entry.pack()
+        Label(self, text="", bg="white", fg="black").pack()
+        Label(self, text="Password * ", bg="white", fg="black").pack()
+        password__login_entry = tk.Entry(self, textvariable=password_login, show= '*', bg="white", fg="black")
+        password__login_entry.pack()
+        Label(self, text="", bg="white", fg="black").pack()
+
+        # Set login button
+        login_button = tk.Button(self, text='Login', height="2", width="20",
+        command=lambda: controller.show_frame(HomePage))
+        login_button.pack(pady=10)
 
         back_button = tk.Button(self, text='Go back', height="2", width="20", 
         command=lambda: controller.show_frame(HomePage))
@@ -97,6 +116,33 @@ class Page_Register(tk.Frame):
 
         text3_label = tk.Label(self, text="E-Library Register", bg="white", fg="black", font=('Helvetica', '32', 'bold'))
         text3_label.pack(pady=10,padx=10)
+
+        username = StringVar()
+        password = StringVar()
+
+        # Set label for user's instruction
+        register_label = tk.Label(self, text="Please enter details below", bg="white", fg="black", 
+        font=('Helvetica', '16'))
+        register_label.pack(pady=10)
+        # Set username label
+        username_lable = tk.Label(self, text="Username * ", bg="white", fg="black")
+        username_lable.pack()
+        # Set username entry
+        username_entry = tk.Entry(self, textvariable=username, bg="white", fg="black")
+        username_entry.pack()
+        # Set password label
+        password_lable = tk.Label(self, text="Password * ", bg="white", fg="black")
+        password_lable.pack()
+        # Set password entry
+        password_entry = tk.Entry(self, textvariable=password, show='*', bg="white", fg="black")
+        password_entry.pack()
+        
+        Label(self, text="", bg="white").pack()
+        
+        # Set register button
+        register_button = tk.Button(self, text='Register', height="2", width="20",
+        command=lambda: controller.show_frame(HomePage))
+        register_button.pack(pady=10)
 
         back_button = tk.Button(self, text='Go back', height="2", width="20",
         command=lambda: controller.show_frame(HomePage))
