f41cfa6b901fb7aa99de73d6f2381439f2d3d034
Nea Träskman <nea.traskman@gmail.com>
2022-04-03 19:49:21 +0300

    Added background images to all pages

--- Code/Gui_class.py
+++ Code/Gui_class.py
@@ -13,10 +13,10 @@ class App(tk.Tk):
         #importing image
         #global image
        
-        img = PhotoImage(file="C:/Users/Melina/Desktop/ATURKUOPINNOT/oop/ex_work/exercise-work/Code/library_photo.png")
+        #img = PhotoImage(file="C:/Users/Melina/Desktop/ATURKUOPINNOT/oop/ex_work/exercise-work/Code/library_photo.png")
 
-        label1 = Label( self, image = img)
-        label1.place(x = 0, y = 0)
+        #label1 = Label( self, image = img)
+        #label1.place(x = 0, y = 0)
 
         #creating a container
         container = tk.Frame(self)
@@ -28,7 +28,7 @@ class App(tk.Tk):
         self.frames = {}
 
         #Defining frames and packing it
-        for F in (HomePage, Page_Login):
+        for F in (HomePage, Page_Login, Page_Register):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -56,16 +56,17 @@ class HomePage(tk.Frame):
         bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
         # text label
-        text_label = tk.Label(self, text="E-Library", font=('Helvetica', '32', 'bold'))
+        text_label = tk.Label(self, text="E-Library", bg="white", fg="black", font=('Helvetica', '32', 'bold'))
         text_label.pack(pady=10,padx=10)
 
         # creating the login button
-        Button_Login = tk.Button(text="Login", height="2", width="20",
+        Button_Login = tk.Button(self, text="Login", height="2", width="20",
         command=lambda: controller.show_frame(Page_Login))
         Button_Login.pack(pady=10)
 
         # creating the register button
-        Button_Register = tk.Button(text="Register", height="2", width="20")
+        Button_Register = tk.Button(self, text="Register", height="2", width="20",
+        command=lambda: controller.show_frame(Page_Register))
         Button_Register.pack(pady=10)
 
 
@@ -74,11 +75,32 @@ class Page_Login(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
-        label = tk.Label(self, text="E-Library Login", font=('Helvetica', '32', 'bold'))
-        label.pack(pady=10,padx=10)
+        # image label
+        bg_label = tk.Label(self, image = img)
+        bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-        back_button = tk.Button(self, text='Go back', 
+        text2_label = tk.Label(self, text="E-Library Login", bg="white", fg="black", font=('Helvetica', '32', 'bold'))
+        text2_label.pack(pady=10,padx=10)
+
+        back_button = tk.Button(self, text='Go back', height="2", width="20", 
         command=lambda: controller.show_frame(HomePage))
         back_button.pack(pady=10)
+
+class Page_Register(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        # image label
+        bg_label = tk.Label(self, image = img)
+        bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+        text3_label = tk.Label(self, text="E-Library Register", bg="white", fg="black", font=('Helvetica', '32', 'bold'))
+        text3_label.pack(pady=10,padx=10)
+
+        back_button = tk.Button(self, text='Go back', height="2", width="20",
+        command=lambda: controller.show_frame(HomePage))
+        back_button.pack(pady=10)
+
 
 
