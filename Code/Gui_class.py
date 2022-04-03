9c318e060766e3187ab94ce0947006113a150e54
Nea Träskman <nea.traskman@gmail.com>
2022-04-03 14:52:10 +0300

    Updated Gui_class.py
    You're now able to press the Login button and it switches "pages", it is still quite buggy but works.

--- Code/Gui_class.py
+++ Code/Gui_class.py
@@ -1,29 +1,68 @@
+import tkinter as tk
 from tkinter import *
 
-class App(Tk):
-
+class App(tk.Tk):
     def __init__(self):
-        super().__init__()
+        tk.Tk.__init__(self)
 
-        self.geometry("612x459") # setting the size of the GUI window
-        self.title("E-Library - Login") # title of the GUI window
+        self.geometry("600x400") # setting the size of the GUI window
+        self.title("E-Library") # title of the GUI window
         self.resizable(False, False)
 
-        global bg
-        bg = PhotoImage(file = "Code/library_photo.png")
+        #importing image
+        #global image
+        #image = PhotoImage(file = "Code/library_photo.png")
 
-        self.label = Label(self, image= bg)
-        self.label.place(x=0, y=0, relwidth=1, relheight=1)
+        #creating a container
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-        # Adding a text box on top of our image
-        self.text_box = Label(self, text="E-Library", font="helvetica 32 bold", fg="black", bg = "white")
-        self.text_box.pack(pady=30)
+        #Initialize frames
+        self.frames = {}
+
+        #Defining frames and packing it
+        for F in (HomePage, Page_Login):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+        
+        self.show_frame(HomePage)
+
+    def show_frame(self, page):
+        frame = self.frames[page]
+        frame.tkraise()
+
+
+class HomePage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        label = tk.Label(self, text="E-Library", font=('Helvetica', '32', 'bold'))
+        label.pack(pady=10,padx=10)
 
         # creating the login button
-        self.button_login = Button(text="Login", height="2", width="20")
-        self.button_login.pack(pady=20)
+        Button_Login = tk.Button(text="Login", height="2", width="20",
+        command=lambda: controller.show_frame(Page_Login))
+        Button_Login.pack(pady=10)
 
         # creating the register button
-        self.button_register = Button(text="Register", height="2", width="20")
-        self.button_register.pack(pady=20)
+        Button_Register = tk.Button(text="Register", height="2", width="20")
+        Button_Register.pack(pady=10)
+
+
+class Page_Login(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        label = tk.Label(self, text="E-Library Login", font=('Helvetica', '32', 'bold'))
+        label.pack(pady=10,padx=10)
+
+        back_button = tk.Button(self, text='Go back', 
+        command=lambda: controller.show_frame(HomePage))
+        back_button.pack(pady=10)
+
 
