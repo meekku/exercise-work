a174521490f44aa7eedfca726f762bb36fbaa2d4
Nea Träskman <nea.traskman@gmail.com>
2022-04-05 10:44:59 +0300

    Added user registration
    Code creates a txt file but still need to figure it out how to put the user information within the file

--- Code/Gui_class.py
+++ Code/Gui_class.py
@@ -109,8 +109,8 @@ class Page_Register(tk.Frame):
         text3_label = tk.Label(self, text="E-Library Register", bg="white", fg="black", font=('Helvetica', '32', 'bold'))
         text3_label.pack(pady=10,padx=10)
 
-        username = StringVar()
-        password = StringVar()
+        self.username = StringVar()
+        self.password = StringVar()
 
         # Set label for user's instruction
         register_label = tk.Label(self, text="Please enter details below", bg="white", fg="black", 
@@ -120,28 +120,41 @@ class Page_Register(tk.Frame):
         username_lable = tk.Label(self, text="Username * ", bg="white", fg="black")
         username_lable.pack()
         # Set username entry
-        username_entry = tk.Entry(self, textvariable=username, bg="white", fg="black")
-        username_entry.pack()
+        self.username_entry = tk.Entry(self, textvariable=self.username, bg="white", fg="black")
+        self.username_entry.pack()
         # Set password label
         password_lable = tk.Label(self, text="Password * ", bg="white", fg="black")
         password_lable.pack()
         # Set password entry
-        password_entry = tk.Entry(self, textvariable=password, show='*', bg="white", fg="black")
-        password_entry.pack()
+        self.password_entry = tk.Entry(self, textvariable=self.password, show='*', bg="white", fg="black")
+        self.password_entry.pack()
         
         Label(self, text="", bg="white").pack()
         
         # Set register button
         register_button = tk.Button(self, text='Register', height="2", width="20",
-        command=lambda: controller.show_frame(HomePage))
+        command=User_Validation)
         register_button.pack(pady=10)
 
         back_button = tk.Button(self, text='Go back', height="2", width="20",
         command=lambda: controller.show_frame(HomePage))
         back_button.pack(pady=10)
 
-class User_Validation(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
+class User_Validation(Page_Register):
+    def __init__(self, parent="", controller=""):
+        Page_Register.__init__(self, parent="", controller="")
+
+        # get the username and password
+        get_username = self.username.get()
+        get_password = self.password.get()
+
+        user_file = open("Code/user_information.txt", "w")
+
+        user_file.write(get_username + "\n")
+        user_file.write(get_password)
+        print("Closing file.")
+        user_file.close()
+
+        self.username_entry.delete(0, END)
+        self.password_entry.delete(0, END)
 
