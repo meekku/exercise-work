2100858993cfacae62ef6b167beca4bcc6848bd9
Nea Träskman <nea.traskman@gmail.com>
2022-04-18 15:04:43 +0300

    Updated main's texts

--- Code/main.py
+++ Code/main.py
@@ -1,16 +1,17 @@
 # File-name: main.py
-# Author: Melina Kamunen and Nea Träskman
-# Description: main.py acts as a startup file to open the program code and GUI interface about the e-library.In e-library you can create users that can log in to the service, where you can borrow books, movies and magazines. The program also gives you access to see and return your own loans, and look at your profile.
-
+# Authors: Melina Kamunen and Nea Träskman
+# Description: main.py acts as a startup file to open the program code and 
+# GUI interface about the e-library.In e-library you can create users that can 
+# log in to the service, where you can borrow books, movies and magazines. 
+# The program also gives you access to see and return your own loans, 
+# and look at your profile.
 
 import Gui_class
 
 def main():
     
-
     app = Gui_class.App()
     app.mainloop()
-
 
 if __name__ == "__main__":
     main()
